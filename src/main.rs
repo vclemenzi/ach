79fbafdf5b67e3ebd15a7dc34d028c4854809df3
vclemenzi <vclemenzi@users.noreply.github.com>
2023-08-19 08:40:59 +0200

    fix!: cache management commands error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::{Arg, Command};
 
-mod package;
-mod log;
 mod cache;
 mod deps;
+mod log;
+mod package;
 mod utils;
 
 fn main() {
@@ -23,30 +23,22 @@ fn main() {
                         .short('i')
                         .num_args(1)
                         .long("info")
-                        .help("Get information about a package")
-                )
+                        .help("Get information about a package"),
+                ),
         )
         .subcommand(
             Command::new("sync")
                 .short_flag('S')
                 .long_flag("sync")
                 .about("Install a package from the AUR")
-                .arg(
-                    Arg::new("package")
-                        .help("The packages")
-                        .num_args(1..)
-                )
+                .arg(Arg::new("package").help("The packages").num_args(1..)),
         )
         .subcommand(
             Command::new("remove")
                 .short_flag('R')
                 .long_flag("remove")
                 .about("Remove an installed package from the AUR")
-                .arg(
-                    Arg::new("package")
-                        .help("The packages")
-                        .num_args(1..)
-                )
+                .arg(Arg::new("package").help("The packages").num_args(1..)),
         )
         .subcommand(
             Command::new("cache")
@@ -58,7 +50,7 @@ fn main() {
                         .short('c')
                         .num_args(0)
                         .long("clear")
-                        .help("Clear the cache")
+                        .help("Clear the cache"),
                 )
                 .arg(
                     Arg::new("remove")
@@ -66,8 +58,8 @@ fn main() {
                         .num_args(1..)
                         .conflicts_with("clear")
                         .long("remove")
-                        .help("Remove a package in the cache")
-                )
+                        .help("Remove a package in the cache"),
+                ),
         )
         .get_matches();
 
@@ -106,37 +98,40 @@ fn main() {
             // Cache
             cache::create_cache();
 
-            let clear: bool = smatches
-                .get_flag("clear");
-
-            let remove: Vec<_> = smatches
-                .get_many::<String>("remove")
-                .expect("is present")
-                .map(|s| s.as_str())
-                .collect();
+            let clear: bool = smatches.get_flag("clear");
 
             if clear {
                 cache::clear();
-            } if !remove.is_empty() {
+
+                return;
+            }
+
+            let remove: Vec<_> = smatches
+                .get_many::<String>("remove")
+                .unwrap_or_default()
+                .map(|s| s.as_str())
+                .collect();
+
+            if !remove.is_empty() {
                 if utils::confirm("Do you want to delete this package(s)") {
                     for package in remove {
                         cache::remove(package.to_string());
                     }
 
-                   log::cache_package_removed(); 
+                    log::cache_package_removed();
                 }
-            } else {
-                cache::list();
+
+                return;
             }
+
+            cache::list();
         }
 
         Some(("query", smatches)) => {
             // Cache
             cache::create_cache();
 
-            let info = smatches
-                .get_one::<String>("info")
-                .expect("is present");
+            let info = smatches.get_one::<String>("info").expect("is present");
 
             package::info(info.to_string());
         }
@@ -144,4 +139,3 @@ fn main() {
         _ => unreachable!(),
     }
 }
-
